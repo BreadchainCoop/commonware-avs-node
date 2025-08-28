e566d6f3a2b2930be416661139d79384583b49fb
junkicide <ajinkyak@protonmail.com>
2025-08-29 00:50:13 +0200

    chore: cleanup

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,5 +1,4 @@
 mod contributor;
 pub mod traits;
-// mod aggregating_contributor;
 pub use contributor::{AggregationInput, Contributor};
-// pub use aggregating_contributor::AggregatingContributor;
+
